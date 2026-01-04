c505798b6e2417e386560ded64ecee7ba269c60f
Nehuen1304 <nehuen.guevara@mi.unc.edu.ar>
2026-01-04 16:26:21 -0300

    fix: remove broken submodules links

--- dotc-backend/tests/test_dependencies.py
+++
@@ -1,7 +0,0 @@
-from app.main import app
-from app.dependencies.dependencies import setup_dependencies, IGameManager
-
-
-def test_setup_dependencies_sets_override():
-    setup_dependencies(app)
-    assert IGameManager in app.dependency_overrides
